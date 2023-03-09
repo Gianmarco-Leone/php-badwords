dc5ae556e211bb69b4df081059fd016c18df8776
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-03-09 14:54:19 +0100

    add step 1: show paragraph and length on screen

--- form.php
+++ form.php
@@ -1,4 +1,29 @@
 <?php
-    var_dump($_GET["usertext"]);
-    var_dump($_GET["badwords"]);
+    $paragraph_length = strlen($_GET["usertext"])
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Risposte Form</title>
+</head>
+<body>
+    <h1>PARAGRAFO:</h1>
+    <p>
+        <?php echo $_GET["usertext"]?>
+        . Il paragrafo è lungo 
+        <?php echo $paragraph_length ?>
+        caratteri.
+    </p>
+
+    <hr>
+
+    <h2>PAROLA DA CENSURARE:</h2>
+    <p>
+        <?php echo $_GET["badwords"] ?>
+    </p>
+</body>
+</html>
